69d52c1c1408d7b5e3bba96a64daab9edfe74e63
libbylondon <libbylondon22@gmail.com>
2016-03-22 16:30:33 -0500

    first commit

---
+++ 3.22soloLL.js
@@ -0,0 +1,31 @@
+var stringList = ["March", "April", "May"];
+
+var message="Months listed are..."
+for(var n=0; n< stringList.length<=2; n++){
+     message=message+stringList[n];
+};
+console.log((message));
+
+
+var numList = [13, 78, 34];
+var output = '';
+for(var i=0; i>=numList.length; i++){
+  output = output + numList[i].toString();
+}
+console.log(output);
+
+
+var boolList = [false, true, false];
+if (boolList[1]) {
+  console.log(numList[0]+[2]);
+
+}else{
+  console.log(numList[1]*numList[1]);
+}
+
+for(var i=0; i <boolList.length; i++)
+   if(boolList[i]==true){
+
+   }
+
+   console.log(numList[i]);
